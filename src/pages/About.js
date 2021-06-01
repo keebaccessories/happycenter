aa2f32667a22ad383dd815597384ef143bd9a85d
mrquan0908vn <mrquan0908vn@gmail.com>
2021-06-01 12:05:45 +0700

    Revert "Revert "Update""
    This reverts commit ca17b03ebe3bebe52432cce66b647ebacc29d31e.

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import LayoutCenter from '../layout/LayoutCenter'
+
+function About() {
+    return (
+        <LayoutCenter>
+            <div>
+            Giảng Viên
+        </div>
+    
+        </LayoutCenter>
+        )
+}
+
+export default About
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,34 @@
 import React from 'react'
-import Search from '../components/Search';
-import TopMenu from '../components/TopMenu';
-
-function Home() {
+import LayoutCenter from '../layout/LayoutCenter'
+import About from './About'
+ 
+const Home = () => {
     return (
-        <div className="bg-blue-100 h-full">
-
-            <div className="flex justify-center pt-20 pb-20">
-                <img className="w-1/4" src="./assets/images/text.png"/>
-            </div>
-            <Search/>
-            <TopMenu/>
+        <div>
+            <LayoutCenter>
+                <section>
+                    <About/>
+                </section>
+            </LayoutCenter>
         </div>
     )
 }
 
 export default Home
+
+
+
+// function Home() {
+//     return (
+//         <div className="flex flex-col bg-blue-100 h-full">
+
+//             <div className="flex justify-center pt-20 pb-20">
+//                 <img className="w-1/4" src="./assets/images/text.png"/>
+//             </div>
+//             <SearchBox/>
+//             <Tab/>
+//         </div>
+//     )
+// }
+
+// export default Home
